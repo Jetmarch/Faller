c541bbb684ab5bc3ba2602f79372375aceb1ba5c
Jet <j3tmarch@gmail.com>
2022-03-19 19:50:48 +0300

    Initial commit

---
+++ Assets/Scripts/AxisMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisMove : MonoBehaviour
+{
+    public enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    public Axis moveOnAxis;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveOnAxis == Axis.X)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0f, 0f));
+        }
+
+        if(moveOnAxis == Axis.Y)
+        {
+            transform.Translate(new Vector3(0f, speed * Time.deltaTime, 0f));
+        }
+
+        if(moveOnAxis == Axis.Z)
+        {
+            transform.Translate(new Vector3(0f, 0f, speed * Time.deltaTime));
+        }
+    }
+}
